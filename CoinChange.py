288c32ee24620e10ba2e56cecb5ef383b0715523
Harsha_784 <98533767+harshavardhan784@users.noreply.github.com>
2022-11-02 15:05:12 +0530

    Add files via upload

---
+++ CoinChange.py
@@ -0,0 +1,23 @@
+def knapsack(W,w,n):
+    if( w==0):
+        return 1
+    if(w<0):
+        return 0
+    if(n<=0):
+        return 0
+    return max(knapsack(W,w-W[n-1],n),knapsack(W,w,n-1))
+    # elif(W[n-1]>w):
+    #     return(knapsack(W,w,n-1))
+
+w=int(input("Coin:"))
+n=int(input("No of denominations:"))
+W=[]
+print("Denominations array-")
+for i in range(n):
+    W.append(int(input()))
+i,j=(n+1,w+1)
+t=[[int(-1)]*j]*i
+if(knapsack(W,w,n)==1):
+    print("Change can be given")
+else:
+    print("Change cannot be given")
